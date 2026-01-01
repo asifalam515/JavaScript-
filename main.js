8cdacadd28ae31fb8b22564d72328667d8926e0a
asifalam515 <asibulalam515@gmail.com>
2026-01-01 21:51:22 +0600

    update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.log("whatare you doing ")
 const age=23
 if (age>18) {
-  console.log("you are adult")
+  console.log('just thik about it')
 }
 else {
   console.console.log("you are child")
